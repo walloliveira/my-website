bf06f34238db11be82afbcb48b662b8845164175
Walber Jr. de Oliveira <walberjr.oliveira@gmail.com>
2023-02-20 11:58:23 -0300

    Criando a secção About (#9)

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,7 @@
 <script setup lang="ts">
+import About from "./components/About.vue";
 import AppBarMenu from "./components/AppBarMenu.vue";
+import Home from "./components/Home.vue";
 </script>
 
 <template>
@@ -10,52 +12,15 @@ import AppBarMenu from "./components/AppBarMenu.vue";
         <VSpacer />
         <AppBarMenu />
       </VAppBar>
-      <VMain scrollable>
+      <VMain>
         <VContainer class="app-container">
-          <VCarousel hide-delimiters cycle :show-arrows="false" height="100%">
-            <VCarouselItem>
-              <div class="d-flex align-stretch">
-                <div class="d-flex align-center">
-                  <div class="text-h1">
-                    I'm a
-                    <span class="text-primary">software engineer</span>
-                    from Brazil
-                  </div>
-                </div>
-                <VImg
-                  src="https://preview.colorlib.com/theme/niko/images/about.jpg.webp"
-                  cover
-                  width="480px"
-                />
-              </div>
-            </VCarouselItem>
-            <VCarouselItem>
-              <div class="d-flex align-stretch">
-                <div class="d-flex align-center">
-                  <div>
-                    <div class="text-subtitle-1 font-weight-bold text-info">
-                      Hello
-                    </div>
-                    <div class="text-h1">
-                      I'm
-                      <span class="text-primary">Walber Oliveira</span>
-                    </div>
-                  </div>
-                </div>
-                <VImg
-                  src="https://preview.colorlib.com/theme/niko/images/bg_2.jpg.webp"
-                  cover
-                  width="768px"
-                />
-              </div>
-            </VCarouselItem>
-          </VCarousel>
+          <Home style="height: 90vh" />
+          <About style="height: 90vh" />
         </VContainer>
       </VMain>
     </VApp>
   </VNoSsr>
 </template>
-
 <style scoped lang="scss">
 .app-container {
   max-width: 1200px;
