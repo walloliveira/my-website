f1d2a1e4bf865725c3c144f8e6fc28f8346901b5
Walber Oliveira <walberjr.oliveira@gmail.com>
2023-02-23 17:15:57 -0300

    Alterando textos na secção Footer

--- src/views/Footer.vue
+++ src/views/Footer.vue
@@ -20,46 +20,15 @@ const socialNetworks = ref([
   <VSheet color="primary" :elevation="6">
     <VRow class="mt-16 mb-16">
       <VCol cols="8" offset="2">
-        <div>
-          <VRow>
-            <VCol>
-              <p class="text-h5 mb-6">Lets talk about</p>
-              <p class="text-caption">
-                Far far away, behind the word mountains, far from the countries
-                Vokalia and Consonantia, there live the blind texts.
-              </p>
-            </VCol>
-            <VCol>
-              <p class="text-h5 mb-6">Links</p>
-              <VBtn variant="text" prepend-icon="mdi-link">Home</VBtn>
-            </VCol>
-            <VCol>
-              <p class="text-h5 mb-6">Services</p>
-              <VBtn variant="text" prepend-icon="mdi-link">Home</VBtn>
-            </VCol>
-            <VCol>
-              <p class="text-h5 mb-6">Have a Questions?</p>
-              <div class="d-flex align-center mt-1 mb-1">
-                <VIcon icon="mdi-map-marker" />
-                <p class="text-caption ml-1">
-                  Blumenau, Santa Catarina, Brazil
-                </p>
-              </div>
-              <div class="d-flex align-center mt-1 mb-1">
-                <VIcon icon="mdi-phone" />
-                <p class="text-caption ml-1">+55 44991668179</p>
-              </div>
-              <div class="d-flex align-center mt-1">
-                <VIcon icon="mdi-email" />
-                <p class="text-caption ml-1">walberjr.oliveira@gmail.com</p>
-              </div>
-            </VCol>
-          </VRow>
-          <VRow justify="center">
-            <VCol
+        <div class="d-flex align-center justify-center flex-column">
+          <p class="text-h5 mb-6">
+            {{ $t("footer.socialNetworks") }}
+          </p>
+          <div class="d-flex">
+            <div
               v-for="socialNetwork in socialNetworks"
               :key="socialNetwork.icon"
-              cols="1"
+              class="ml-3"
             >
               <VBtn
                 color="secondary"
@@ -67,16 +36,46 @@ const socialNetworks = ref([
                 :href="socialNetwork.url"
                 target="_blank"
               />
-            </VCol>
-          </VRow>
-          <VRow class="text-center">
-            <VCol>
-              <p class="text-caption">
-                Copyright ©2023 All rights reserved | This template is made with
-                by WallOliveira.dev
-              </p>
-            </VCol>
-          </VRow>
+            </div>
+          </div>
+        </div>
+        <div class="d-flex flex-column align-center mt-6">
+          <div>
+            <div class="d-flex align-center mt-1 mb-1">
+              <VBtn
+                icon="mdi-map-marker"
+                href="https://goo.gl/maps/2t4c1cypcUDmXXvf6"
+                target="_blank"
+                variant="text"
+              />
+              <p class="text-caption ml-1">Blumenau, Santa Catarina, Brazil</p>
+            </div>
+            <div class="d-flex align-center mt-1 mb-1">
+              <VBtn
+                icon="mdi-phone"
+                variant="text"
+                href="tel:+5544991668179"
+                target="_blank"
+              />
+              <p class="text-caption ml-1">+5544991668179</p>
+            </div>
+            <div class="d-flex align-center mt-1">
+              <VBtn
+                icon="mdi-email"
+                variant="text"
+                href="email:walberjr.oliveira@gmail.com"
+                target="_blank"
+              />
+              <p class="text-caption ml-1">walberjr.oliveira@gmail.com</p>
+            </div>
+          </div>
+        </div>
+        <div class="text-center">
+          <VCol cols="12">
+            <p class="text-caption">
+              {{ $t("footer.copyright") }}
+            </p>
+          </VCol>
         </div>
       </VCol>
     </VRow>
