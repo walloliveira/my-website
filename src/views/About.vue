b7d5782447c0b7146249b84e964d9f0ec4b12382
Walber Oliveira <walberjr.oliveira@gmail.com>
2023-02-23 12:08:12 -0300

    Alterando textos nas secções Home e About

--- src/views/About.vue
+++ src/views/About.vue
@@ -1,49 +1,48 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import { ref } from "vue";
+import AboutMe1 from "../assets/about-me-1.webp";
+
+const roles = ref([
+  {
+    title: "about.roles[0].title",
+    text: "about.roles[0].text",
+    icon: "mdi-application-braces-outline",
+  },
+  {
+    title: "about.roles[1].title",
+    text: "about.roles[1].text",
+    icon: "mdi-application-cog-outline",
+  },
+  {
+    title: "about.roles[2].title",
+    text: "about.roles[2].text",
+    icon: "mdi-cloud-cog",
+  },
+]);
+</script>
 
 <template>
   <VRow class="mt-16 mb-16">
     <VCol cols="4">
-      <VImg
-        src="https://preview.colorlib.com/theme/niko/images/about-1.jpg.webp"
-      ></VImg>
+      <VImg :src="AboutMe1" />
     </VCol>
     <VCol cols="8">
       <VRow>
         <VCol cols="12">
           <div class="d-flex flex-column">
-            <p class="text-subtitle-1">Welcome</p>
-            <p class="text-h2">About me</p>
+            <p class="text-subtitle-1">{{ $t("about.subtitle") }}</p>
+            <p class="text-h2">{{ $t("about.title") }}</p>
             <p class="text-body-1 text-medium-emphasis mt-6">
-              A small river named Duden flows by their place and supplies it
-              with the necessary regelialia. It is a paradisematic country, in
-              which roasted parts of sentences fly into your mouth.
+              {{ $t("about.text") }}
             </p>
           </div>
         </VCol>
-        <VCol cols="4">
+        <VCol cols="4" v-for="role in roles" :key="role.title">
           <div class="d-flex flex-column align-stretch">
-            <VIcon icon="mdi-application-braces-outline" size="124px" />
-            <p class="text-subtitle-1">Front end developer</p>
+            <VIcon :icon="role.icon" size="124px" />
+            <p class="text-subtitle-1">{{ $t(role.title) }}</p>
             <p class="text-body-1 text-medium-emphasis mt-6">
-              A small river named Duden flows by their place and supplies.
-            </p>
-          </div>
-        </VCol>
-        <VCol cols="4">
-          <div class="d-flex flex-column align-stretch">
-            <VIcon icon="mdi-application-cog-outline" size="124px" />
-            <p class="text-subtitle-1">Back end developer</p>
-            <p class="text-body-1 text-medium-emphasis mt-6">
-              A small river named Duden flows by their place and supplies
-            </p>
-          </div>
-        </VCol>
-        <VCol cols="4">
-          <div class="d-flex flex-column align-stretch">
-            <VIcon icon="mdi-cloud-cog" size="124px" />
-            <p class="text-subtitle-1">Cloud architect</p>
-            <p class="text-body-1 text-medium-emphasis mt-6">
-              A small river named Duden flows by their place and supplies.
+              {{ $t(role.text) }}
             </p>
           </div>
         </VCol>
