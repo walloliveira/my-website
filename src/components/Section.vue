94866b16cb8573c5ac45a17549bc2bc721219372
Walber Oliveira <walberjr.oliveira@gmail.com>
2023-02-23 15:05:57 -0300

    Alterando textos na secção Skills

--- src/components/Section.vue
+++ src/components/Section.vue
@@ -1,21 +1,31 @@
 <script setup lang="ts">
+import { computed } from "vue";
+
 interface SectionTitleProps {
   subtitle: string;
   title: string;
   text: string;
+  removeMarginBottom?: boolean;
+  removeMarginTop?: boolean;
 }
 
 const props = defineProps<SectionTitleProps>();
+
+const sectionClass = computed(() => {
+  let mt = `${props.removeMarginTop ? "" : "mt-16"}`;
+  let mb = `${props.removeMarginBottom ? "" : "mb-16"}`;
+  return `${mt} ${mb}`;
+});
 </script>
 
 <template>
   <div class="d-flex flex-column text-center">
-    <p class="text-subtitle-1 text-uppercase">{{ props.subtitle }}</p>
-    <p class="text-h2">{{ props.title }}</p>
+    <p class="text-subtitle-1 text-uppercase">{{ $t(props.subtitle) }}</p>
+    <p class="text-h2">{{ $t(props.title) }}</p>
     <p class="text-body-1 text-medium-emphasis mt-6">
-      {{ props.text }}
+      {{ $t(props.text) }}
     </p>
-    <div class="mt-16 mb-16">
+    <div :class="sectionClass">
       <slot></slot>
     </div>
   </div>
--- src/plugins/VueI18n.ts
+++ src/plugins/VueI18n.ts
@@ -2,12 +2,18 @@ import { createI18n } from "vue-i18n";
 import enUs from "../locales/en-US.json";
 import ptBr from "../locales/pt-BR.json";
 
+const parseToObject = (obj: object) => JSON.parse(JSON.stringify(obj));
+
 const i18n = createI18n({
   locale: "pt-BR",
   legacy: false,
   messages: {
-    "en-US": enUs,
-    "pt-BR": ptBr,
+    "en-US": enUs.messages,
+    "pt-BR": ptBr.messages,
+  },
+  datetimeFormats: {
+    "en-US": parseToObject(enUs.datetimeFormats),
+    "pt-BR": parseToObject(ptBr.datetimeFormats),
   },
 });
 
