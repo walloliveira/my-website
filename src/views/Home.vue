d05032094c6f1409646886bd2319a937bc31418f
Walber Oliveira <walberjr.oliveira@gmail.com>
2023-02-24 12:10:41 -0300

    Adicionando bandeiras na selecao da lingua

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -12,20 +12,6 @@ import Me2 from "../assets/me-2.webp";
       height="65vh"
     >
       <VCarouselItem>
-        <div class="d-flex align-center">
-          <div class="d-flex align-center">
-            <div class="text-h1">
-              {{ $t("home.carouselItem1[0]") }}
-              <span class="text-primary">
-                {{ $t("home.carouselItem1[1]") }}
-              </span>
-              {{ $t("home.carouselItem1[2]") }}
-            </div>
-          </div>
-          <VImg :src="Me1" cover width="480px" />
-        </div>
-      </VCarouselItem>
-      <VCarouselItem>
         <div class="d-flex align-stretch">
           <div class="d-flex align-center">
             <div>
@@ -41,6 +27,20 @@ import Me2 from "../assets/me-2.webp";
             </div>
           </div>
           <VImg :src="Me2" cover width="768px" />
+        </div>
+      </VCarouselItem>
+      <VCarouselItem>
+        <div class="d-flex align-center">
+          <div class="d-flex align-center">
+            <div class="text-h1">
+              {{ $t("home.carouselItem1[0]") }}
+              <span class="text-primary">
+                {{ $t("home.carouselItem1[1]") }}
+              </span>
+              {{ $t("home.carouselItem1[2]") }}
+            </div>
+          </div>
+          <VImg :src="Me1" cover width="480px" />
         </div>
       </VCarouselItem>
     </VCarousel>
